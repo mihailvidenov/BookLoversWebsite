91cfbe7856da75458f826e3137a1631df3886be6
hp <hp@DESKTOP-2ESMHC5>
2022-11-28 22:47:17 +0200

    Add project files.

---
+++ BookLoversWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookLoversWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
